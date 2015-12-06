7d614b45284e5956746bce6e4bdecbd3c1e5c839
Yuuki Harano <masm@masm11.ddo.jp>
2015-12-06 13:50:53 +0900

    とりあえず nil が返る関数。

--- Makefile
+++ Makefile
@@ -7,6 +7,6 @@ all:
 	rm rbxfce4-*.gem
 	gem build rbxfce4.gemspec
 
-	rm -fr $HOME/.gem/ruby/2.2.0/gems/rbxfce4-*
-	rm -fr $HOME/.gem/ruby/2.2.0/extensions/x86_64-linux/2.2.0/rbxfce4-0.0.1/rbxfce4/rbxfce4.so
-	gem install ./rbxfce4-0.0.1.gem
+	rm -fr $(HOME)/.gem/ruby/2.2.0/gems/rbxfce4-*
+	rm -fr $(HOME)/.gem/ruby/2.2.0/extensions/x86_64-linux/2.2.0/rbxfce4-*
+	gem install ./rbxfce4-*.gem
--- ext/rbxfce4/main.c
+++ ext/rbxfce4/main.c
@@ -9,6 +9,11 @@ static void construct_func(void)
 XFCE_PANEL_PLUGIN_REGISTER_EXTERNAL(construct_func)
 #undef main
 
+static VALUE rbxfce4_register(void)
+{
+    return Qnil;
+}
+
 void
 Init_rbxfce4(void) {
     VALUE mXfce4;
@@ -16,6 +21,7 @@ Init_rbxfce4(void) {
 //  mXfce4 = rb_const_get(rb_cObject, rb_intern("Xfce4"));
     mXfce4 = rb_define_module("Xfce4");
 
+    rb_define_module_function(mXfce4, "register", rbxfce4_register, 0);
 //  rb_define_alloc_func(cMyMalloc, my_malloc_alloc);
 //  rb_define_method(cMyMalloc, "initialize", my_malloc_init, 1);
 //  rb_define_method(cMyMalloc, "free", my_malloc_release, 0);
